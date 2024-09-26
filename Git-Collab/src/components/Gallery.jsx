7cd677b11491ff205a58a2a7471ecd5e7811ccfa
skaravinth <skaravinth790@gmail.com>
2024-09-26 11:53:17 +0530

    modified gallery page

--- Git-Collab/src/components/Gallery.jsx
+++ Git-Collab/src/components/Gallery.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Home from './Home';
+
 
 
 const Gallery = () => {
   return (
     <div className="gallery">
-      <h2>Gallery Page
-        <Home/>
+      <h2>this is a gallery page
+        
       </h2>
     </div>
   );
